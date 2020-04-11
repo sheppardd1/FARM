d2b29fd398e7c29bdb18d3bb8e75a58c331c6828
David <sheppardd1@students.rowan.edu>
2020-04-11 14:03:54 -0400

    checked select field switch to a checkbox

--- app/src/main/java/com/example/davea/FARM/GetInterval.java
+++ app/src/main/java/com/example/davea/FARM/GetInterval.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -30,12 +31,11 @@ public class GetInterval extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_interval);
 
-
         //setup UI
         final EditText interval_input = findViewById(R.id.interval_input);
         final EditText width_input = findViewById(R.id.width_input);
         Button done = findViewById(R.id.done);
-        Switch drawFieldSw = findViewById(R.id.drawFieldSwitch);
+        CheckBox drawFieldCheckBox = findViewById(R.id.drawFieldCheckBox);
         locationChoiceRadioGroup = findViewById(R.id.LocationChoiceRadioGroup);
         mapChoiceRadioGroup = findViewById(R.id.MapChoiceRadioGroup);
 
@@ -92,13 +92,9 @@ public class GetInterval extends AppCompatActivity {
             }
         });
 
-        drawFieldSw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        drawFieldCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    drawField = true;
-                } else {
-                    drawField = false;
-                }
+                drawField = isChecked;
             }
         });
     }
